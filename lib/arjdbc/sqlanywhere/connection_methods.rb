b5bd27321c9e26d6199d61994bdb4e03771d7589
Brian Olsen <brian@maven-group.org>
2011-10-17 21:31:12 +0200

    Added driver version detection to connection setup

--- lib/arjdbc/sqlanywhere/connection_methods.rb
+++ lib/arjdbc/sqlanywhere/connection_methods.rb
@@ -1,9 +1,15 @@
 class ActiveRecord::Base
   class << self
     def sqlanywhere_connection( config )
+      version4 = true
+      begin
+        Java::com.sybase.jdbc4.jdbc.SybDriver
+      rescue NameError
+        version4 = false
+      end
       config[:port] ||= 2638
       config[:url] ||= "jdbc:sybase:Tds:#{config[:host]}:#{config[:port]}?serviceName=#{config[:database]}"
-      config[:driver] ||= "com.sybase.jdbc3.jdbc.SybDriver"
+      config[:driver] ||= version4 ? "com.sybase.jdbc4.jdbc.SybDriver" : "com.sybase.jdbc3.jdbc.SybDriver"
       config[:dialect] = "sqlanywhere"
       jdbc_connection(config)
     end
