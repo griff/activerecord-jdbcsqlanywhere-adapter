619baa13172489ddff9c948e1468ef5b359b1348
Brian Olsen <brian@maven-group.org>
2011-10-17 21:30:22 +0200

    Renamed :server connection config key to :host

--- lib/arjdbc/sqlanywhere/connection_methods.rb
+++ lib/arjdbc/sqlanywhere/connection_methods.rb
@@ -2,7 +2,7 @@ class ActiveRecord::Base
   class << self
     def sqlanywhere_connection( config )
       config[:port] ||= 2638
-      config[:url] ||= "jdbc:sybase:Tds:#{config[:server]}:#{config[:port]}?serviceName=#{config[:database]}"
+      config[:url] ||= "jdbc:sybase:Tds:#{config[:host]}:#{config[:port]}?serviceName=#{config[:database]}"
       config[:driver] ||= "com.sybase.jdbc3.jdbc.SybDriver"
       config[:dialect] = "sqlanywhere"
       jdbc_connection(config)
